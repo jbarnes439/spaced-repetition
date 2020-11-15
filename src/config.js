c79373adb4bf8ee108f89a78afe89d6c9cff6010
Josh Barnes <jbarnes439@gmail.com>
2020-11-15 10:19:47 -0700

    prepared for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: 'https://rocky-badlands-01441.herokuapp.com/api',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
